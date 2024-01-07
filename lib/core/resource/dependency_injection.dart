db84c1f34349cf5ee55a91032c3ab72693acb83e
khondakar.afridi <afridi.khondakar@gmail.com>
2024-01-07 09:43:37 +0600

    Added network controller in DI

--- lib/core/resource/dependency_injection.dart
+++ lib/core/resource/dependency_injection.dart
@@ -1,9 +1,11 @@
+import 'package:clean_mvc_template/config/network_service/network_controller.dart';
 import 'package:clean_mvc_template/config/network_service/network_service.dart';
 import 'package:get/get.dart';
 
 class DependencyInjection extends Bindings {
   @override
   void dependencies() {
+    Get.put<NetworkController>(NetworkController(), permanent: true);
     Get.put<HttpService>(HttpService(), permanent: true);
   }
 }
