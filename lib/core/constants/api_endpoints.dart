f41bc514c42b1b263f0dffef6c6894028a6e51b9
Khondakar Afridi <afridi.khondakar@gmail.com>
2024-12-10 10:17:44 +0600

    Api logic updated

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,3 +1,10 @@
+const String baseUrl = 'https://yourstore.com/wp-json/wc/v3'; // Replace with your store URL
+const String consumerKey = 'your_consumer_key'; // Replace with your Consumer Key
+const String consumerSecret = 'your_consumer_secret'; // Replace with your Consumer Secret
+
+// final String url =
+//         '$baseUrl/orders?consumer_key=$consumerKey&consumer_secret=$consumerSecret';
+
 class ApiEndPoints {
   static const String baseUrl = "";
   static const String login = "/login";
